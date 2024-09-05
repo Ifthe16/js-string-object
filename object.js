c06523eafc44f94ae9728176f0b9fbf20dc558b2
Ifthe16 <habiburrahmanif975@gmail.com>
2024-09-06 01:16:46 +0600

    added object, property and value file of the javascript

---
+++ object.js
@@ -0,0 +1,40 @@
+// primitive
+// basic, primary
+const age = 21;
+const school = 'rifle square';
+const isPassed = true;
+// let isDeveloper;
+// console.log(isDeveloper);
+
+const subjects = ['bangla', 'English', 'Physics', 'Math']
+const bottle_wrong = ['white', 45];
+const bottleColor = 'white';
+const bottlePrice = 45;
+const bottleDrinks = 'water';
+
+// object
+// non-primitive
+const bottle = {
+    brand: 'apple',
+    price: 45,
+    color: 'white',
+    isClean: false
+}
+
+console.log(bottle);
+
+// non-primitive
+const subject = {
+    name: 'biology',
+    teacher: 'rasheda mam',
+    examDate: '30Feb',
+    chapters: ['first', 'second', 'third'],
+    exams: {
+        name: 'Final Exam',
+        marks: 100
+    }
+}
+console.log(subject);
+
+
+
